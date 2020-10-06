18469a82c5d5280aeb16541a3c1694b6eb49bca7
Ezequiel Campos Pereira <zecaobh@gmail.com>
2020-10-05 23:13:32 -0300

    del

--- DijkstraBE_MatlabCode/DifferentialEvolution/getIndividuoCorrente.m
+++
@@ -1,8 +0,0 @@
-% get individuo corrente
-function individuo = getIndividuoCorrente(alim)
-
-% individuo corrente
-individuo = alim.FmBranch(:,11)';
-
-end
-
--- DijkstraBE_MatlabCode/DifferentialEvolution/plotaInfoAG2.m
+++
@@ -1,9 +0,0 @@
-
-% plota informacoes do algoritmo genetico no arquivo txt
-function plotaInfoAG2( algoritmo, sistema, numexec, matrizResultados )
-
-% salva arquivo
-FILENAME = strcat('InfoAG',num2str(algoritmo),'_',num2str(sistema),'_',num2str(numexec),'.txt');
-save(FILENAME,'matrizResultados','-ascii','-tabs');
-
-end
