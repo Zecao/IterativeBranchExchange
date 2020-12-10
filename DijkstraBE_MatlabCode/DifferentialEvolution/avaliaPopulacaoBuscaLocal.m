0664b7562b02c64e2371cffb1bc184819de36d1d
Ezequiel Campos Pereira <zecaobh@gmail.com>
2020-12-09 22:37:55 -0300

    Create avaliaPopulacaoBuscaLocal.m

---
+++ DijkstraBE_MatlabCode/DifferentialEvolution/avaliaPopulacaoBuscaLocal.m
@@ -0,0 +1,42 @@
+% Avalia a funcao objetivo para a Populacao.
+function [ fitness, alim, results ] = avaliaPopulacaoBuscaLocal(populacao, alim)
+
+% init var
+results = [];
+
+tamPopulacao = size(populacao,1);
+
+% opcao fluxo 
+if ( ~ strcmp(alim.funcao,'fluxo') )
+
+    % decodifica populacao binaria 
+    populacao = decodificaPopBinario(populacao, alim.funcao);
+
+end
+
+for i=1:tamPopulacao
+    
+    individuo = populacao(i, :);
+    
+    if (strcmp(alim.Ftipo,'ieee'))
+    
+        [avaliadoBool, fit, results] = getStatusAvaliacaoInd(individuo);
+    
+    end
+    
+    % verifica se individuo ja foi avaliado
+    if ( ~avaliadoBool )
+
+        [ fit, alim, results ] = avaliaIndividuoBuscaLocal( individuo, alim);
+    
+    end
+
+    % preenche fitness
+    fitness(i,1) = fit;
+    
+end
+
+end
+
+
+
