1c8bd1d29220f627aa565efec756236fd2b8cd8f
Ezequiel Campos Pereira <zecaobh@gmail.com>
2021-05-23 15:35:07 -0300

    Update lstAlimentadores.m

--- CemigDFeeders/0PermRes/lstAlimentadores.m
+++ CemigDFeeders/0PermRes/lstAlimentadores.m
@@ -1,6 +1,22 @@
+JFAU07
+% PRRU07
+% NOG02
+% AXAU03
+% TOTU03
+% IIGU115
+% MCDD206
+% JTA02
+% AXAU03
+% AMN06
+% PSAU13
+% SFIQ410
+% ANAD08
+% UHQM06
+% PMSU15
+% MCLD203
 % PMSU15 % não reproduz 2019
 % MCLD203 % não reproduz 2019
 % LBD01 % LBD02/05 % problema inv BRT
-MAL08 % 
+% MAL08 % nao reproduz 2019
 % UHQM06 % nao reproduz 2019
 % MOO06
