2b4b8ec640bea1a02eca204628b48787775e9205
Ezequiel Campos Pereira <zecaobh@gmail.com>
2023-01-09 20:03:46 -0300

    Test

---
+++ Teste/AuxClasses/FeederMetering.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+
+namespace ExecutorOpenDSS.Classes_Auxiliares
+{
+
+    public class FeederMetering
+    {
+        public GeneralParameters _paramGerais;
+
+        //booleanas para o controle se arquivo Excel foi carregado
+        public bool _reqEnergiaMesCarregado = false;
+
+        //public bool _mapTensaoBarramentoCarregado = false;
+        public bool _reqDemandaMaxCarregado = false;
+
+        // map demanda max X alim
+        internal Dictionary<string, double> _mapDadosDemanda;
+
+        // map energia injetada X alim
+        internal MonthlyEnergy _reqEnergiaMes;
+
+        // Load MultMes
+        internal MonthLoadMult _reqLoadMultMes;
+
+        //contrutor
+        public FeederMetering(GeneralParameters par)
+        {
+            _paramGerais = par;
+
+            _reqEnergiaMes = new MonthlyEnergy(par);
+        }
+
+        public void CarregaDados()
+        {
+            // Arquivo de loadMUlt sempre sera carregado
+            _paramGerais._mWindow.ExibeMsgDisplay("Carregando arquivo de LoadMults...");
+
+            // Carrega map com os valores dos loadMult por alimentador
+            _reqLoadMultMes = new MonthLoadMult(_paramGerais);
+                 
+            // carrega arquivo de requisito uma unica vez
+            if ((_paramGerais._mWindow._parGUI._otmPorEnergia) && (!_reqEnergiaMesCarregado))
+            {
+                _paramGerais._mWindow.ExibeMsgDisplay("Carregando arquivo de requisitos de energia...");
+
+                // Carrega map com valores de energia mes do alimentador
+                _reqEnergiaMes.CarregaMapEnergiaMesAlim();
+
+                _reqEnergiaMesCarregado = true;
+            }
+
+            // carrega maps conforme necessidade
+            if ((_paramGerais._mWindow._parGUI._otmPorDemMax) && (!_reqDemandaMaxCarregado))
+            {
+                _paramGerais._mWindow.ExibeMsgDisplay("Carregando arquivo de LoadMults demanda...");
+
+                // Carrega map com valores de demanda maxima do alimentador
+                _paramGerais._medAlim.CarregaMapDemandaMaxAlim();
+
+                _reqDemandaMaxCarregado = true;
+            }
+        }
+
+        // Load map from Excel file with demand values
+        private void CarregaMapDemandaMaxAlim( )
+        {
+            // TODO refactory 
+            string nomeArquivoCompleto = _paramGerais._parGUI._pathRecursosPerm + _paramGerais._arqDemandaMaxAlim;
+
+            _paramGerais._medAlim._mapDadosDemanda = XLSXFile.XLSX2Dictionary(nomeArquivoCompleto);
+        }
+
+        internal void AtualizaMapAlimLoadMult(double loadMult)
+        {
+            _reqLoadMultMes.AtualizaMapAlimLoadMult(_paramGerais.GetNomeAlimAtual(), loadMult, _paramGerais._parGUI.GetMes());
+        }
+    }
+}
---
+++ Teste/AuxClasses/TxtFile.cs
@@ -0,0 +1,149 @@
+﻿using ExecutorOpenDSS.Classes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ExecutorOpenDSS
+{
+    class TxtFile
+    {
+        //Verifica se o arquivo existe antes de deletá-lo
+        public static void SafeDelete(string arquivo)
+        {
+            if (File.Exists(arquivo))
+            {
+                File.Delete(arquivo);
+            }
+        }
+        //Grava CONTEUDO em arquivo FID 
+        public static void GravaEmArquivo2(string conteudo, string fid, MainWindow janela)
+        {
+            try
+            {
+                File.AppendAllText(fid, conteudo);
+            }
+            catch
+            {
+                janela.ExibeMsgDisplay("Não foi possível escrever no arquivo " + fid);
+            }
+        }
+
+        //Escreve uma lista de strings em um arquivo
+        public static void GravaListArquivoTXT(List<string> conteudo, string arquivo, MainWindow jan)
+        {
+            //Verifica se existe conteudo
+            if (conteudo != null)
+            {
+                //Tenta escrever no arquivo
+                try
+                {
+                    File.AppendAllLines(arquivo, conteudo);
+                }
+
+                //Caso não consiga, exibe mensagem de erro
+                catch
+                {
+                    
+                    jan.ExibeMsgDisplay("Não foi possível escrever no arquivo " + arquivo);
+                }
+            }
+        }
+        
+        // OLD CODE
+        // Grava CONTEUDO em arquivo FID 
+        public static void GravaEmArquivoAsync(string conteudo, string fid, MainWindow jan)
+        {
+            //
+            while (true)
+            {
+                try
+                {
+                    // TODO consertar esquema de bloqueio de arquivo
+                    string arq = jan._indiceArq == 0 ? fid : fid + jan._indiceArq + ".txt";
+
+                    using (StreamWriter file = new StreamWriter(arq, true))
+                    {
+                        file.WriteLineAsync(conteudo);
+                    }
+                    break;
+                }
+                catch
+                {
+                    jan.ExibeMsgDisplay("Arquivo " + fid + " bloqueado. Tentando outro nome de arquivo");
+                    jan._indiceArq++;
+                }
+                if (jan._indiceArq > 10)
+                {
+                    jan.ExibeMsgDisplay("Não foi possível escrever o arquivo " + fid + ". Verifique se possui permissão de escrita no local");
+                    break;
+                }
+            }
+        }
+
+        //Grava CONTEUDO em arquivo FID 
+        public static void GravaEmArquivo(string conteudo, string fid, MainWindow jan )
+        {
+            //
+            while(true)
+            {
+                try
+                {
+                    // TODO consertar esquema de bloqueio de arquivo
+                    string arq = jan._indiceArq == 0 ? fid : fid + jan._indiceArq + ".txt";
+                    
+                    using (StreamWriter file = new StreamWriter(arq, true))
+                    {
+                        file.WriteLineAsync(conteudo);
+                    }
+                    break;
+                }
+                catch 
+                {
+                        jan.ExibeMsgDisplay("Arquivo " + fid + " bloqueado. Tentando outro nome de arquivo");
+                        jan._indiceArq++;
+                }
+                if (jan._indiceArq > 10)
+                {
+                    jan.ExibeMsgDisplay("Não foi possível escrever o arquivo " + fid + ". Verifique se possui permissão de escrita no local");
+                    break;
+                }
+            }       
+        }
+
+        //funcao gravaPerdas em arquivo
+        static public void GravaPerdas(PFResults perdasTotais, string nomeAlim, string fid, MainWindow jan)
+        {
+            // cria string com o formato de saida das perdas
+            string conteudo = perdasTotais._energyMeter.FormataResultado(nomeAlim);
+
+            //Grava em arquivo
+            TxtFile.GravaEmArquivo(conteudo, fid, jan);
+        }
+ 
+        //Grava a lista de alimentadores não convergentes em um txt
+        static public void GravaLstAlimNaoConvergiram(GeneralParameters paramGerais)
+        {
+            string nomeAlim = paramGerais.GetNomeAlimAtual();
+
+            while (true)
+            {
+                try
+                {
+                    // 
+                    string arquivoNome = paramGerais.GetNomeComp_arquivoResAlimNaoConvergiram();
+                    
+                    using (StreamWriter file = new StreamWriter(arquivoNome, true))
+                    {
+                        file.WriteLineAsync(nomeAlim);
+                    }
+                    break;
+                }
+                catch
+                {
+                    paramGerais._mWindow.ExibeMsgDisplay("Arquivo dos alimentadores não convergentes bloqueado!");
+                }
+            }       
+        }
+    
+    }
+}
