9d48351d84035bb0ed328c73a09fb12486c29aad
Ezequiel Campos Pereira <zecaobh@gmail.com>
2023-01-09 19:34:47 -0300

    MinorFixes

--- CemigDFeeders/0PermRes/lstAlimentadores.m
+++ CemigDFeeders/0PermRes/lstAlimentadores.m
@@ -1,22 +1,4 @@
-JFAU07
-% PRRU07
-% NOG02
-% AXAU03
-% TOTU03
-% IIGU115
-% MCDD206
-% JTA02
-% AXAU03
-% AMN06
-% PSAU13
-% SFIQ410
-% ANAD08
-% UHQM06
-% PMSU15
-% MCLD203
-% PMSU15 % não reproduz 2019
-% MCLD203 % não reproduz 2019
-% LBD01 % LBD02/05 % problema inv BRT
-% MAL08 % nao reproduz 2019
-% UHQM06 % nao reproduz 2019
-% MOO06
+PSAU13
+IIGD210
+IIGU115
+CETU04
