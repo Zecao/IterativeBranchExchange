b83d3b9a540a78f7f5b5ba827d357116020f1958
Ezequiel Campos Pereira <zecaobh@gmail.com>
2023-01-09 19:44:06 -0300

    ExecutorDSS

--- IBE_MatlabCode/setPath.m
+++ IBE_MatlabCode/setPath.m
@@ -1,7 +1,12 @@
 function setPath()
 
+% strPath = '';
+
+path(path,pwd);
 path(path,strcat(pwd,'\BranchExchange'));
 path(path,strcat(pwd,'\DifferentialEvolution'));
+path(path,strcat(pwd,'\matlab_bgl'));
+path(path,strcat(pwd,'\matpower7.0'));
 path(path,strcat(pwd,'\Networks'));
 
-end
+end 
