24a92ae2a330988805463b06f9fe799ebc3f2ddb
Ezequiel Campos Pereira <zecaobh@gmail.com>
2024-11-01 01:44:24 -0300

    updateLastVersion

--- IBE_MatlabCode/Metaheuristics/iterativeBranchExchange521.m
+++
@@ -1,7 +0,0 @@
-% 
-function [ populacao, fxi ] = iterativeBranchExchange521(alim, populacao, fxi, geracao )
-
-% busca local
-[populacao, fxi] = buscaLocal(populacao,fxi,alim,geracao);
-
-end
--- IBE_MatlabCode/Networks/getNumVarsAlimentador.m
+++
@@ -1,7 +0,0 @@
-% retorna numero de variaveis totais alimentador 
-function nVars = getNumVarsAlimentador(funcao,alim)
-
-%retorna o numero de variaveis e dominio(range) dado o nome da funcao
-[rangeFunction, nVars] = rangeFuncao(funcao,alim);
-
-end
